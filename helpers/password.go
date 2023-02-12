5886f0bf41cc39a425f2defe2d630222471f16dd
Tegar Pratama <tegar.pratama@kanggo.id>
2023-02-12 20:48:51 +0700

    update helper

--- helpers/password.go
+++ helpers/password.go
@@ -4,11 +4,8 @@ import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
 
-	return string(passwordHash), nil
+	return string(passwordHash), err
 }
 
 func VerifyPassword(hashPassword, password string) error {
